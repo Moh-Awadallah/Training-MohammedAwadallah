6755ab973ce78eb8981c1e13f019be0ebb8e0643
mohammad awadallah <mohammad@mohammad-awadallah-pc.scoiumtech.local>
2022-08-17 11:23:09 +0300

    finished the first code challenge

---
+++ codeChallenges/codeChallenge1/index.test.js
@@ -0,0 +1,35 @@
+
+/**
+ * Given a single input array, write a function that outputs an unique array of values
+ *
+ *
+ * What time complexity is your solution?
+ * 
+ *  the time complexity is the number of times you iterate through the input array so O(n)
+ *
+ * Your solution shouldn't using any build in function.
+ *
+ * Credit: Write a unit tests.
+ */
+/**
+  * example usage:
+  * const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+  * console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
+  */
+const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+const getUniqueValues = (arrOfNum) => {
+    let arr = [];  // create an empty array
+    for (let i=0, l=arrOfNum.length; i<l; i++) // iterate through
+        if (arr.indexOf(arrOfNum[i]) === -1) // using indexOf we check if the number is in the new array
+            arr.push(arrOfNum[i]); // if not then we push it
+    return arr; // reutn the array
+};
+console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
+console.log(getUniqueValues([1 , 2 , 1 , 4]));
+
+// expect(getUniqueValues(arrOfNum)).toEqual([1, 2, 4 , 5 , 6]);
+// expect(getUniqueValues([1 , 1 , 1 , 1 , 1 ])).toEqual([1]);
+// expect(getUniqueValues([1, 2 , 3 , 4]).toEqual([1 , 2 , 3 , 4]))
+
+
+
