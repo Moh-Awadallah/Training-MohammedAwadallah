c9147e493c8e34f2f20b8db691131e31369f0c18
mohammad awadallah <mohammad@mohammad-awadallah-pc.scoiumtech.local>
2022-08-18 10:48:34 +0300

    Challenge2  : A function that returns the Factorial of a number

---
+++ codeChallenges/codeChallenge2/index.test.js
@@ -0,0 +1,32 @@
+/**
+ * The factorial of a number is the product of all the numbers from 1 to that number. For example,
+ * factorial of 5 is equal to 1 * 2 * 3 * 4 * 5 = 120.
+ * The factorial of a positive number n is given by:
+ * factorial of n (n!) = 1 * 2 * 3 * 4.....n
+ * Write a function that, given a number, returns a factorial of the number
+ */
+function factorial(number) {
+    let fact = Math.abs(number);
+    if (number == 0 || number == 1) return 1; //base cases
+    if (number > 1) fact = fact * factorial(number - 1);
+        return fact;
+}
+// describe("Tests", () => {
+//     it("test factorial", () => {
+//         expect(factorial(0)).toEqual(1);
+//         expect(factorial(1)).toEqual(1);
+//         expect(factorial(5)).toEqual(120);
+//         expect(factorial(6)).toEqual(720);
+//         expect(factorial(11)).toEqual(39916800);
+//     });
+// });
+
+// The time complexity of the solution is O(n)
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(5));
+console.log(factorial(6));
+console.log(factorial(11));
+
+
